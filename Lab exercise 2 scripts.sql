431fc0e9f047e8cdc53869f2f8b1c48789647440
Andrew Marfo <andrew.marfo@amalitech.com>
2024-11-20 22:29:47 +0000

    index creation for the joins and aggregation queries for optimization

--- Lab exercise 2 scripts.sql
+++ Lab exercise 2 scripts.sql
@@ -55,3 +55,48 @@ FROM sales_data;
 
 -- Lab Exercise 2.3
 
+-- Analyzing and optimizing the Joins Operations query
+USE shop_ease_new;
+
+EXPLAIN ANALYZE SELECT *
+FROM orders
+JOIN customers ON orders.customer_id = customers.customer_id
+JOIN products ON orders.product_id = products.product_id
+ORDER BY orders.order_id;
+ 
+ -- Index creation for optimization
+CREATE INDEX idx_customers_customer_id ON customers(customer_id);
+CREATE INDEX idx_orders_order_id ON orders(customer_id);
+CREATE INDEX idx_product_id ON products(product_id);
+CREATE INDEX idx_orders_product_id ON orders(product_id);
+CREATE INDEX idx_order_id ON orders(order_id);
+
+EXPLAIN ANALYZE SELECT *
+FROM orders
+JOIN customers ON orders.customer_id = customers.customer_id
+JOIN products ON orders.product_id = products.product_id
+ORDER BY orders.order_id;
+
+
+-- Analyzing and optimizing the subqueries
+
+EXPLAIN ANALYZE SELECT product_name, SUM(total_revenue) as total_sales
+FROM sales_data
+WHERE year = (SELECT MAX(year) FROM sales_data)
+AND month = (SELECT MAX(month) FROM sales_data WHERE year = (SELECT MAX(year) FROM sales_data))
+GROUP BY product_name 
+ORDER BY total_sales DESC
+LIMIT 5;
+
+-- Index creation for optimization
+CREATE INDEX idx_year_month ON sales_data(year, month);
+CREATE INDEX idx_sales_data_product_name ON sales_data(product_name);
+CREATE INDEX idx_total_revenue ON sales_data(total_revenue);
+
+EXPLAIN ANALYZE SELECT product_name, SUM(total_revenue) as total_sales
+FROM sales_data
+WHERE year = (SELECT MAX(year) FROM sales_data)
+AND month = (SELECT MAX(month) FROM sales_data WHERE year = (SELECT MAX(year) FROM sales_data))
+GROUP BY product_name 
+ORDER BY total_sales DESC
+LIMIT 5;
